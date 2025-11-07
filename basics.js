c2767ab59a10ca56b86cddb730300cb6d1875c80
Eldin Hasic <e.hasic.2007@gmail.com>
2025-11-07 10:37:29 +0100

    task 3 & bonus task added

--- basics.js
+++ basics.js
@@ -1,16 +1,34 @@
 
-function fullName(name, surname) { // Vor und Nachname mit Abstand schreiben
+function fullName(name, surname) { // Name and surname with space between
     let value = (name.concat(" ", surname));
     return value;
 }
 
-console.log(fullName("Eldin","Hasic"));
+console.log(fullName("Eldin", "Hasic"));
 // Task 1 done
 
-function capitalizeLetters(capstr) { // Strings GROSSSCHREIBEN
+function capitalizeLetters(capstr) { // Strings CAPSLOCK
     let value = (capstr.toUpperCase());
     return value;
-} 
+}
 
 console.log(capitalizeLetters("baNaNe"));
 // Task 2 done
+
+function countCharacters(digitcount) { // digit-count
+    let value = (digitcount.length);
+    return value;
+}
+
+console.log(countCharacters("banana"));
+// Task 3 done
+
+
+function formatToCurrency(num) { // number to currency converter with 2 decimal places
+    let value = (num.toLocaleString("de-DE", {style:"currency", currency:"EUR"}));
+    num = num.toFixed(2);
+    return value;
+}
+
+console.log(formatToCurrency(33550336.345));
+// Bonus Task done
