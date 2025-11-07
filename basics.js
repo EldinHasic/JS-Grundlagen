4772836a277a3a743f735391701f86a159b84ac9
Eldin Hasic <e.hasic.2007@gmail.com>
2025-11-07 09:34:08 +0100

    task 2 & notes added

--- basics.js
+++ basics.js
@@ -1,7 +1,16 @@
 
-function fullName(name, surname) {
+function fullName(name, surname) { // Vor und Nachname mit Abstand schreiben
     let value = (name.concat(" ", surname));
     return value;
 }
 
 console.log(fullName("Eldin","Hasic"));
+// Task 1 done
+
+function capitalizeLetters(capstr) { // Strings GROSSSCHREIBEN
+    let value = (capstr.toUpperCase());
+    return value;
+} 
+
+console.log(capitalizeLetters("baNaNe"));
+// Task 2 done
